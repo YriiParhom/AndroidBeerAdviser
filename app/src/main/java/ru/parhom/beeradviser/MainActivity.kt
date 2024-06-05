2507dc6fde39bd63520c664d71c0a55cee607df9
Iurii Parkhomenko <i.parkhomenko@astondevs.ru>
2024-06-05 15:04:18 +0300

    дописал приложение

--- app/src/main/java/ru/parhom/beeradviser/MainActivity.kt
+++ app/src/main/java/ru/parhom/beeradviser/MainActivity.kt
@@ -1,20 +1,33 @@
 package ru.parhom.beeradviser
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val findBeer = findViewById<Button>(R.id.find_beer)
+        findBeer.setOnClickListener {
+            val beerColor = findViewById<Spinner>(R.id.beer_color)
+            val color = beerColor.selectedItem
+            val beerList = getBeers(color.toString())
+            val beers = beerList.reduce{str, item -> str + '\n' + item}
+            val brands = findViewById<TextView>(R.id.brands)
+            brands.text = beers
+        }
+    }
+
+    fun getBeers(color: String) : List<String> {
+        return when (color) {
+            "Светлое" -> listOf("Jail Pal Elle", "Lager Lite")
+            "Пшеничное" -> listOf("Jack Amber", "Red Moose")
+            "Тёмное" -> listOf("Brown Bear Beer", "Bock Brownie")
+            else -> listOf("Gout Stout", "Dark Daniel")
         }
     }
 }
